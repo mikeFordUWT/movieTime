afe5a0e6ecd9a8d08042a99feac40b084056cb8c
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-03-11 11:45:13 -0800

    fun stuff

--- Movies/src/data/DatabaseAccess.java
+++ Movies/src/data/DatabaseAccess.java
@@ -1,9 +1,19 @@
 package data;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 public class DatabaseAccess {
 	
 	static DBConnection db = new DBConnection(""
 			+ "jdbc:mysql://fordm13.vergil.u.washington.edu:1138/movies");
 	
-	
+	public ArrayList<Movie> getMovies(String title){
+		try{
+			db.getMovieDetails(title);
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
