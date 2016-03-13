ad08807b764bd2c848bdc9e247d1dad666f271a2
fordm13@uw.edu <husky6tcss360@gmail.com>
2016-03-12 22:54:55 -0800

    jqvuq

--- Movies/src/data/DatabaseAccess.java
+++ Movies/src/data/DatabaseAccess.java
@@ -13,6 +13,7 @@ public class DatabaseAccess {
 	static DBConnection db = new DBConnection(""
 			+ "jdbc:mysql://fordm13.vergil.u.washington.edu:1138/movies");
 
+	//
 	public static ArrayList<Movie> getMovies(String title){
 		try{
 			return db.getMovieDetails(title);
@@ -26,22 +27,37 @@ public class DatabaseAccess {
 		return db.getUser(eMail);
 	}
 
+	/*
+	 * Retrieves user's favorites.
+	 */
 	public static ArrayList<String> getFavs(String userID) throws SQLException {
 		return db.getFavFromUser(userID);
 	}
 
+	/*
+	 * Retrives a user's watch list.
+	 */
 	public static ArrayList<String> getWatch(String userID) throws SQLException{
 		return db.getWatchFromUser(userID);
 	}
 	
+	/*
+	 * Inserts rating from user.
+	 */
 	public static void InsertRating(String userID, String movieID, int rating) throws SQLException{
 		db.addNewRating(userID, movieID, rating);
 	}
 
+	/*
+	 * Add movie to user's watch list.
+	 */
 	public static void AddToWatchInsert(String userID, String movieID) throws SQLException{
 		db.addToWatchList(userID, movieID);
 	}
 	
+	/*
+	 * Adds a movie to user's favorite list.
+	 */
 	public static void addToFavoritesUpdate(String userID, String movieID) throws SQLException{
 		db.addToFavorites(userID, movieID);
 	}
